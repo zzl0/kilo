942f54f0117b22c608e53774cd241bc8eeea4c78
zzl0 <zhuzhaolong0@gmail.com>
2019-03-30 15:55:39 -0400

    turn off canonical mode

--- kilo.c
+++ kilo.c
@@ -1,3 +1,27 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
+
+struct termios orig_termios;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+    tcgetattr(STDERR_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);
+
+    tcsetattr(STDERR_FILENO, TCSAFLUSH, &raw);
+}
+
 int main() {
+    enableRawMode();
+
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
 }
