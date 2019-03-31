ecd456855eb846013292f0523da6fdac35d0a73e
zzl0 <zhuzhaolong0@gmail.com>
2019-03-30 22:58:04 -0400

    refresh screen

--- kilo.c
+++ kilo.c
@@ -66,12 +66,21 @@ void editorProcessKeypress() {
     }
 }
 
+/*** output ***/
+
+void editorRefreshScreen() {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+
 /*** init ***/
 
 int main() {
     enableRawMode();
 
     while (1) {
+        editorRefreshScreen();
         editorProcessKeypress();
     }
 
