f859949a051c91b92ec547d03c3aad9df0226e76
zzl0 <zhuzhaolong0@gmail.com>
2019-04-12 18:39:44 -0400

    complete raw mode

--- python/kilo.py
+++ python/kilo.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import tty
+import copy
 import curses.ascii
 import termios
 import contextlib
@@ -9,12 +10,26 @@ import contextlib
 ### terminal ###
 
 @contextlib.contextmanager
-def raw_mode():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    tty.setraw(fd)
-    yield None
-    termios.tcsetattr(fd, termios.TCSAFLUSH, old_settings)
+def raw_mode(fileno):
+    old_attr = termios.tcgetattr(fileno)
+
+    new_attr = copy.deepcopy(old_attr)
+    new_attr[tty.IFLAG] &= ~(termios.BRKINT | termios.ICRNL | termios.IXON
+                             | termios.INPCK | termios.ISTRIP)
+    new_attr[tty.OFLAG] &= ~(termios.OPOST)
+    new_attr[tty.CFLAG] |= (termios.CS8)
+    new_attr[tty.LFLAG] &= ~(termios.ECHO | termios.ICANON | termios.IEXTEN
+                             | termios.ISIG)
+    # set the number of characters read at a time in non-canonical mode.
+    new_attr[tty.CC][termios.VMIN] = 1
+    # set timeout for read
+    new_attr[tty.CC][termios.VTIME] = 1
+
+    termios.tcsetattr(fileno, termios.TCSAFLUSH, new_attr)
+
+    yield
+
+    termios.tcsetattr(fileno, termios.TCSAFLUSH, old_attr)
 
 
 def main():
@@ -23,11 +38,11 @@ def main():
         if c == 'q':
             return
         elif curses.ascii.iscntrl(c):
-            print(f'{ord(c)}')
+            print(f'{ord(c)}', end='\r\n')
         else:
-            print(f"{ord(c)} ('{c}')", )
+            print(f"{ord(c)} ('{c}')", end='\r\n')
 
 
 if __name__ == '__main__':
-    with raw_mode() as _:
+    with raw_mode(sys.stdin.fileno()):
         main()
